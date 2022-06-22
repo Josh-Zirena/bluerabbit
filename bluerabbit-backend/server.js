c8590aec2e44e8004cb341dbefe4271ecca7eedb
Josh <joshzirena@gmail.com>
2022-06-22 00:49:33 -0400

    feat: init bluerabbit project

---
+++ bluerabbit-backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+const fs = require("fs");
+const multer = require("multer");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/myName", (req, res) => {
+  res.json({ name: "Josh Zirena" });
+});
+
+app.post("/name", (req, res) => {
+  const contents = fs.readFileSync("./output/name.txt", "utf-8");
+  if (!contents) {
+    fs.writeFileSync("./output/name.txt", req.body.name);
+    res.send(req.body);
+  } else {
+    const arr = contents.split(",");
+    arr.push(req.body.name);
+    fs.writeFileSync("./output/name.txt", arr.toString());
+    res.send(arr.toString());
+  }
+});
+
+// Multer File Image Upload
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.post("/processImage", upload.single("image"), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send({ message: "Please upload a file." });
+  }
+  return res.send({ message: "File uploaded successfully.", file });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
---
+++ bluerabbit-frontend/src/pages/Home.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import { Paper, Stack, Input, Typography, TextField, IconButton, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { PhotoCamera } from "@mui/icons-material";
+
+const useStyles = makeStyles({
+  components: {
+    TextField: {
+      styleOverrides: {
+        root: {
+          textAlign: "center",
+        },
+      },
+    },
+  },
+  sections: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: 12,
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  paperRoot: {
+    backgroundColor: "#FFF !important",
+  },
+  centerTextField: {
+    textAlign: "center",
+  },
+});
+
+function Home() {
+  const classes = useStyles();
+  const [myName, setMyName] = useState("");
+  const [displayedName, setDisplayedName] = useState("");
+  const [isNameUploaded, setIsNameUploaded] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/myName").then((res) => res.json());
+      setMyName(res.name);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    setDisplayedName(e.target.value);
+  };
+
+  const uploadImage = (e) => {
+    setSelectedImage(e.target.value);
+    console.log({ selectedImage });
+  };
+
+  const submitImage = async () => {
+    await fetch("http://localhost:8000/processImage", {
+      method: "POST",
+      body: JSON.stringify({ image: selectedImage }),
+    });
+  };
+
+  const handleSubmit = async () => {
+    const input = { name: displayedName };
+    const res = await fetch("http://localhost:8000/name", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(input),
+    });
+    setIsNameUploaded(res.ok);
+  };
+
+  return (
+    <Paper className={classes.paper} classes={{ root: classes.paperRoot }} elevation={3}>
+      {!isNameUploaded && (
+        <>
+          <div className={classes.sections}>
+            <Typography gutterBottom classes={{ root: classes.graphTitle }} variant={"h4"}>
+              {`Hello, my name is ${myName}`}
+            </Typography>
+          </div>
+
+          <div className={classes.sections}>
+            <Typography gutterBottom classes={{ root: classes.graphTitle }} variant={"h8"}>
+              <TextField
+                id="standard-basic"
+                placeholder="What is your name?"
+                variant="standard"
+                inputProps={{ style: { textAlign: "center" } }}
+                sx={{ textAlign: "center" }}
+                onChange={handleChange}
+                value={displayedName}
+              />
+            </Typography>
+          </div>
+          <div className={classes.sections}>
+            <Button onClick={handleSubmit} variant="contained">
+              Submit
+            </Button>
+          </div>
+        </>
+      )}
+
+      {isNameUploaded && (
+        <>
+          <div className={classes.sections}>
+            <Typography gutterBottom classes={{ root: classes.graphTitle }} variant={"h4"}>
+              {`Hi ${displayedName}!`}
+            </Typography>
+          </div>
+          <div className={classes.sections}>
+            <Typography gutterBottom style={{ marginTop: 15 }} classes={{ root: classes.graphTitle }} variant={"h6"}>
+              {`Would you like to upload a picture?`}
+            </Typography>
+          </div>
+
+          <div className={classes.sections}>
+            <div style={{ paddingRight: 9 }}>
+              <Button variant="contained" component="label">
+                <span style={{ paddingRight: 2 }}>UPLOAD</span> <PhotoCamera />
+                <input onChange={uploadImage} accept="image/*" type="file" hidden />
+              </Button>
+            </div>
+            <div>
+              <Button onClick={submitImage} variant="contained">
+                Submit
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
+    </Paper>
+  );
+}
+
+export default Home;
